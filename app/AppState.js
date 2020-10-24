88beeb90656127ed33acfb6bb7fe237812c2653c
Ryan Carpenter <rccarpenter@outlook.com>
2020-10-24 09:30:59 -0600

    models route fix

--- app/AppState.js
+++ app/AppState.js
@@ -1,7 +1,7 @@
-import Image from "./models/Image.js"
+import Image from "./Models/Image.js"
 import Todo from "./Models/Todo.js"
-import Weather from "./models/Weather.js"
-import Quote from "./models/Quote.js"
+import Weather from "./Models/Weather.js"
+import Quote from "./Models/Quote.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
